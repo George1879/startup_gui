e23a937ac0bd39e5f87082ac07de3dcb057b1534
George1879 <81874028+George1879@users.noreply.github.com>
2021-11-06 23:03:29 +0000

    Updated the code to be more customisable

--- startup.py
+++ startup.py
@@ -4,20 +4,18 @@ import os, sys, subprocess
 
 win=Tk()
 
-b1 = Button(win, text = "PREMIERE", command = lambda: os.startfile(r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Adobe Premiere Pro 2021.lnk'))
-b2 = Button(win, text = "STEAM", command = lambda: os.startfile(r'C:\Program Files (x86)\Steam\steam.exe'))
+b1 = Button(win, text = "PREMIERE", command = lambda: os.startfile(r'(file directory here)'))
+b2 = Button(win, text = "STEAM", command = lambda: os.startfile(r'(file directory here)'))
 b3 = Button(win, text = "BRAVE", command = lambda: os.popen('start brave.exe'))
 
 b4 = Button(win, text = "AGE OF WAR", command = lambda: os.popen('start brave https://www.crazygames.com/game/age-of-war'))
 b5 = Button(win, text = "KRUNKER", command = lambda: os.popen('start brave https://krunker.io/?game=FRA:0k2w8'))
-b6 = Button(win, text = "MINECRAFT", command = lambda: os.startfile(r'C:\Users\gdj20\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Lunar Client.lnk'))
-b7 = Button(win, text = "TERRARIA", command = lambda: os.startfile(r'C:\Program Files (x86)\Steam\steamapps\common\Terraria\Terraria.exe'))
+b6 = Button(win, text = "MINECRAFT", command = lambda: os.startfile(r'(file directory here)'))
+b7 = Button(win, text = "TERRARIA", command = lambda: os.startfile(r'(file directory here)'))
 
 b8 = Button(win, text = "SHUTDOWN", command = lambda: subprocess.call(["shutdown.exe", "-f", "-s", "-t", "0"]))
 b9 = Button(win, text = "SLEEP", command = lambda: subprocess.call(['rundll32.exe', 'powrprof.dll', 'SetSuspendState','0','1','0']))
 b10 = Button(win, text = "RESTART", command = lambda: subprocess.call(["shutdown.exe", "-f", "-r", "-t", "0"]))
-b10 = Button(win, text = "???", command = lambda: os.startfile(r'C:\Users\gdj20\OneDrive\Desktop\ok.py'))
-
 
 l = Label(win, text = "Apps")
 k = Label(win, text = "Games")
